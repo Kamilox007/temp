f859c1a37f8933b47beeb1766c6d8ce11ebd038a
Kamil Krzywon <103220086+Kamilox007@users.noreply.github.com>
2024-01-13 17:00:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+import serial
+from spotifyClass import SpotifyClass
 
+spotify = SpotifyRequest
 # Set GPIO mode and setup
 GPIO.setmode(GPIO.BCM)
 
@@ -8,7 +11,7 @@ GPIO.setmode(GPIO.BCM)
 button_pins = [17, 22, 23, 24, 27]
 
 # Setup the serial port
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)  # Change '/dev/ttyS0' to your actual serial port
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # Change '/dev/ttyS0' to your actual serial port
 
 
 # Setup the GPIO pins for input with pull-up resistors
@@ -19,10 +22,19 @@ try:
     print("Button Press Detection. Press Ctrl+C to exit.")
     
     while True:
-        for pin in button_pins:
-            if GPIO.input(pin) == GPIO.LOW:
-                print(f"Button on GPIO {pin} pressed.")
-                time.sleep(1)  # debounce time
+        if GPIO.input(17) == GPIO.LOW:
+            spotify.classInterface("skipToNextRequest")
+
+        if GPIO.input(22) == GPIO.LOW:
+            spotify.classInterface("skipToPreviousRequest")
+        if GPIO.input(23) == GPIO.LOW:
+            response = spotify.classInterface("playRequest")
+            if response.statu_code == 403:
+                spotify.classInterface("stopRequest")
+
+        if GPIO.input(24) == GPIO.LOW:
+
+        if GPIO.input(27) == GPIO.LOW:
 
         serial_data = ser.readline().decode('utf-8').strip()
         if serial_data:

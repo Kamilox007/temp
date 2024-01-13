46a7755ef6a511e9fbabb1947122b912f312d3b3
Kamil Krzywon <103220086+Kamilox007@users.noreply.github.com>
2024-01-13 16:15:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ GPIO.setmode(GPIO.BCM)
 # Define the GPIO pins for buttons
 button_pins = [17, 22, 23, 24, 27]
 
+# Setup the serial port
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)  # Change '/dev/ttyS0' to your actual serial port
+
+
 # Setup the GPIO pins for input with pull-up resistors
 for pin in button_pins:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -18,7 +22,11 @@ try:
         for pin in button_pins:
             if GPIO.input(pin) == GPIO.LOW:
                 print(f"Button on GPIO {pin} pressed.")
-                time.sleep(0.2)  # debounce time
+                time.sleep(1)  # debounce time
+
+        serial_data = ser.readline().decode('utf-8').strip()
+        if serial_data:
+            print(f"Serial Data Received: {serial_data}")
 
 except KeyboardInterrupt:
     print("Exiting...")
